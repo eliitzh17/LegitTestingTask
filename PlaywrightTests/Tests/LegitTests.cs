6edbf7c7a363d1e0f35454eaa3dc003a251f6690
eliitzh17 <32660616+eliitzh17@users.noreply.github.com>
2024-03-20 14:16:05 +0200

    Add bug scenario

--- PlaywrightTests/Tests/LegitTests.cs
+++ PlaywrightTests/Tests/LegitTests.cs
@@ -27,12 +27,12 @@ public class LegitTests : TestBase
             new Product(1, 20, 5),
             new Product(5, 10.5, 2)
         ]);
-        
+
         //Add to shopping cart
         await ShoppingSteps.AddProductsToShoppingCart(page, order.Products);
         await ShoppingSteps.NavigateToShoppingCart(this, page);
         await ShoppingSteps.ValidateProductInShoppingCart(this, page, order.Products);
-        
+
         //Checkout
         order.OrderNumber = await CheckOutSteps.CheckoutFromShoppingCart(page, order.Address);
         await CheckOutSteps.NavigateToOrders(this, page);
@@ -57,18 +57,50 @@ public class LegitTests : TestBase
             new Product(1, 20, 5),
             new Product(5, 10.5, 2)
         ]);
-        
+
         //Add to shopping cart
         await ShoppingSteps.AddProductsToShoppingCart(page, order.Products);
         await ShoppingSteps.NavigateToShoppingCart(this, page);
         await ShoppingSteps.ValidateProductInShoppingCart(this, page, order.Products);
-        
+
         await ShoppingSteps.RemoveProductFromShoppingCartAndValidate(this, page, [order.Products.ElementAt(0)]);
         order.Products.RemoveAt(0);
-        
+
         //Checkout
         order.OrderNumber = await CheckOutSteps.CheckoutFromShoppingCart(page, order.Address);
         await CheckOutSteps.NavigateToOrders(this, page);
         await CheckOutSteps.ValidateOrder(this, page, [order]);
+    }
+
+    [Test]
+    [Ignore("Bug is exist on product pricing, sum-up is incorrect.")]
+    public async Task ValidateProductPricing()
+    {
+        //Prep
+        using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        await using var browser = await InitBrowser(playwright);
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+
+        await page.GotoAsync(Config["HomeAddress"]);
+        await LoginAndRegistrationSteps.Login(page,
+            new Credentials(Config["Username"], Config["Password"]));
+
+        //Scenario start
+        var order = new Order("", $"Bialik {new Random().Next(1, 1500)}, Israel, Ramat Gan", [
+            new Product(2, 30, 2),
+        ]);
+
+        //Add to shopping cart
+        await ShoppingSteps.AddProductsToShoppingCart(page, order.Products);
+        await ShoppingSteps.NavigateToShoppingCart(this, page);
+
+        var productElement = page.GetByText(order.Products.ElementAt(0).ProductName);
+        await Expect(productElement).ToBeVisibleAsync();
+        await Expect(productElement).ToContainTextAsync(order.Products.ElementAt(0).ProductName);
+        await Expect(productElement).ToContainTextAsync("Quantity: " + order.Products.ElementAt(0).ProductQuantity);
+        await Expect(productElement)
+            .ToContainTextAsync("$" + order.Products.ElementAt(0).ProductPrice *
+                order.Products.ElementAt(0).ProductQuantity);
     }
 }
